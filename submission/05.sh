d73314d712a68e5accae1caee45acb79b769d181
dayvvo <dyvvoo@gmail.com>
2026-03-19 09:52:30 +0100

    fix: 05.sjh

--- submission/05.sh
+++ submission/05.sh
@@ -1,5 +1,5 @@
 # Check the total amount in the wallet.
-ADDRESS=$(bitcoin-cli -regtest getaddressesbylabel "builderswallet" | jq -r 'keys[0]')
+WALLET_NAME=$(bitcoin-cli -regtest createwallet "builderswallet")
 
-bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
-bitcoin-cli -regtest getbalance
+bitcoin-cli -regtest generatetoaddress 101 $WALLET_NAME
+bitcoin-cli -regtest getbalance $WALLET_NAME
