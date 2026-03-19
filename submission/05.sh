00a8995979287372b9b8c8e68516064665f3049c
dayvvo <dyvvoo@gmail.com>
2026-03-19 10:40:30 +0100

    fix: rpc wallet balance

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,4 @@
 # Check the total amount in the wallet.
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
-MINED_BLOCKS=$(bitcoin-cli -regtest generatetoaddress 2 $ADDRESS)
-bitcoin-cli -regtest getbalance
+MINED_BLOCKS=$(bitcoin-cli -regtest generatetoaddress 101 $ADDRESS)
+bitcoin-cli -regtest getbalance -rpcwallet=builderswallet
