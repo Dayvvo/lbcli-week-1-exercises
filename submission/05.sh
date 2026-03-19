87a8574be41bc68f893ef0d237fbd3901171ae52
dayvvo <dyvvoo@gmail.com>
2026-03-19 10:33:25 +0100

    fix: balance address as integer

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,4 @@
 # Check the total amount in the wallet.
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
-bitcoin-cli -regtest generatetoaddress 0.005 $ADDRESS
+bitcoin-cli -regtest generatetoaddress 2 $ADDRESS
 bitcoin-cli -regtest getbalance
