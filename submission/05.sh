37b321594d3f88c1ee2b489609c1cf1a1da02871
dayvvo <dyvvoo@gmail.com>
2026-03-19 09:49:57 +0100

    fix: mine block and submit

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,5 @@
 # Check the total amount in the wallet.
+ADDRESS=$(bitcoin-cli -regtest getaddressesbylabel "builderswallet" | jq -r 'keys[0]')
+
+bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
 bitcoin-cli -regtest getbalance
